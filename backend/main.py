39071df08d8b4af2722fc74bcb4e6badb096a000
Moatiz Sabahat <moatiz.sabahat@hotmail.com>
2025-10-20 23:00:10 -0400

    Removed additional variables and only kept speed, brake, throttle, and gear. Increased sample interval to .25 second to further reduce JSON output size (maybe .5 seconds for sweet spot or is that a loss of detail?)

--- backend/main.py
+++ backend/main.py
@@ -11,11 +11,11 @@ LOG_FILE = os.path.join(os.path.dirname(__file__), "telemetry_log.json")
 #we'll store data in memory before writing
 telemetry_data = []
 last_save_time = 0
-save_interval = 5  # seconds (yes it made alot of stuff in the JSON but it shall look sexy once we make a nice final output)
+save_interval = 10  # seconds (yes it made alot of stuff in the JSON but it shall look sexy once we make a nice final output)
 
 # Potential fix to reducing the JSON file size by 67 percent (siiiix seveennnnnnnnn)
 last_sample_time = 0
-sample_interval = 0.1  # seconds (10 samples per second)
+sample_interval = .25  # seconds (10 samples per second)
 
 
 def acMain(ac_version):
@@ -46,14 +46,10 @@ def acUpdate(deltaT):
     last_sample_time = 0
 
     speed = ac.getCarState(0, acsys.CS.SpeedKMH)
-    rpm = ac.getCarState(0, acsys.CS.RPM)
     gear = ac.getCarState(0, acsys.CS.Gear)
     throttle = ac.getCarState(0, acsys.CS.Gas)
     brake = ac.getCarState(0, acsys.CS.Brake)
-    steer = ac.getCarState(0, acsys.CS.Steer)
-    clutch = ac.getCarState(0, acsys.CS.Clutch)
 
-    steering_deg = steer * 450  # assuming 900° wheel rotation which is usually the max for quite literally every car. It comes up to insane values like 20k but every sim I noticed measures that max value even if you're on controller
 
     # gear formatting (Initial-D type shi)
     if gear == 0:
@@ -66,22 +62,17 @@ def acUpdate(deltaT):
     # update UI
     ac.setText(
         speed_label,
-        "Spd:{:.0f} | RPM:{:.0f} | G:{} | Th:{:.0%} | Br:{:.0%} | Cl:{:.0%} | St:{:.0f}°".format(
-            speed, rpm, gear_text, throttle, brake, clutch, steering_deg
+        "Spd:{:.0f} | G:{} | Th:{:.0%} | Br:{:.0%}".format(
+            speed, gear_text, throttle, brake,
         )
     )
 
     # record telemetry data and become Fernando Alonso
     telemetry_data.append({
-        "time": time.strftime("%H:%M:%S"),
         "speed": round(speed, 2),
-        "rpm": int(rpm),
         "gear": gear_text,
         "throttle": round(throttle, 3),
         "brake": round(brake, 3),
-        "clutch": round(clutch, 3),
-        "steer": round(steer, 3),
-        "steer_deg": round(steering_deg, 1)
     })
 
     # save every few seconds (in case of an AC crash, very common especially if you don't have CSP or some PP filter is fucking with ur AC config)
