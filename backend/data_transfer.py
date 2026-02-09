0d625a87974fa64285ebb9378d96f89bbbc16f24
RileySetser <setserr7@gmail.com>
2026-02-09 16:09:17 -0500

    initial commit

---
+++ backend/data_transfer.py
@@ -0,0 +1,7 @@
+# to do:
+# 1.) check for telemetry data JSON
+# 2.) connect to Firestore database
+# 3.) parse and format JSON for sending
+# 4.) send data
+# 5.) delete data JSON
+# 6.) confirm success through pop-up window.
