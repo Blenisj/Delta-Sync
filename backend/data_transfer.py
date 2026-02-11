fe7993a3592c0f9facb0de839edbd2f3ee2dde68
RileySetser <setserr7@gmail.com>
2026-02-10 19:03:21 -0500

    data now transfers to firebase database.

--- backend/data_transfer.py
+++ backend/data_transfer.py
@@ -3,7 +3,7 @@
 # 2.) connect to Firestore database
 # 3.) parse and format JSON for sending
 # 4.) send data
-# 5.) delete data JSON*
+# 5.) delete data JSON* ()
 # 6.) confirm success through pop-up window.
 
 import firebase_admin
@@ -18,6 +18,7 @@ cred = credentials.Certificate("accessKey.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
+# Check to see if file exists. If not, end script.
 def checkForFile():
     file_path = "telemetry_log.json"
     if os.path.exists(file_path):
@@ -27,20 +28,32 @@ def checkForFile():
     else:
         print("File doesn't exist")
 
-def getDatabase():
-    return
-
+# Reads the file (if found) and formats it for the database
 def formatData(_file):
     txt = _file.read()
     obj = json.loads(txt)
-    #print(obj["metadata"]["track_name"])
-    ctypes.windll.user32.MessageBoxW(0, "If you see this, then the batch file worked. :D", obj["metadata"]["track_name"], 1)
-    return
+    data = {
+        "metadata": {
+            "lap_time": obj["metadata"]["best_lap_time_ms"],
+            "track": obj["metadata"]["track_name"],
+            "car": obj["metadata"]["car_name"],
+            "timestamp": obj["metadata"]["last_save_timestamp"],
+            "samples": obj["metadata"]["samples_logged"]
+        },
+        "telemetry": obj["telemetry"]
+    }
+    sendData(data)
 
-def sendData():
-    return
+def sendData(_data):
+    try:
+        data_ref = db.collection("main").document()
+        data_ref.set(_data)
+        confirmWindow()
+    except Exception as e:
+        ctypes.windll.user32.MessageBoxW(0, "Error while sending data: ".format(e), "ERROR", 1)
+    
 
 def confirmWindow():
-    return
+    ctypes.windll.user32.MessageBoxW(0, "Data sent successfully.", "Notice", 1)
 
 checkForFile()
