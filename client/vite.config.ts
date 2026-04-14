67f9fb3af95d930913f4a0275a27632abfad66ca
Blenisj <jblenis99@gmail.com>
2026-04-14 08:59:49 -0400

    update vite

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,4 +6,7 @@ import path from "path";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: { alias: { "@": path.resolve(__dirname, "src") } },
+
+  base: "/Delta-Sync/",
+
 });
